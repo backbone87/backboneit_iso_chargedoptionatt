4e4131ef9a0ba7e4bfb874c22d761079cfd08367
Oliver Hoff <oliver@hofff.com>
2012-12-03 18:52:19 +0100

    remove old iso_config, which is now in the helper extension

--- TL_ROOT/system/modules/backboneit_iso_chargedoptionatt/config/iso_config.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$GLOBALS['ISO_ATTR']['bbit_iso_coa']['backend']	= 'multiColumnWizard';
-$GLOBALS['ISO_ATTR']['bbit_iso_coa']['sql']		= 'blob NULL';
-$GLOBALS['ISO_ATTR']['bbit_iso_coa']['callback']= array(array('IsotopeChargedOptionAttribute', 'callbackCOA'));
-$GLOBALS['ISO_ATTR']['bbit_iso_coa']['class']	= 'FormIsotopeChargedOptionAttribute';
