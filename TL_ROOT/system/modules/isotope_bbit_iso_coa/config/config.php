7662fc122eb4114c553a3e0a8c23b37857c32719
Oliver Hoff <oliver@hofff.com>
2012-12-03 17:15:54 +0100

    optimized config to allow custom attribute callbacks out of load order

--- TL_ROOT/system/modules/isotope_bbit_iso_coa/config/config.php
+++ TL_ROOT/system/modules/isotope_bbit_iso_coa/config/config.php
@@ -1,6 +1,6 @@
 <?php
 
-$GLOBALS['ISO_ATTR']['bbit_iso_coa']['backend']	= 'multiColumnWizard';
-$GLOBALS['ISO_ATTR']['bbit_iso_coa']['sql']		= 'blob NULL';
-$GLOBALS['ISO_ATTR']['bbit_iso_coa']['callback']= array(array('IsotopeChargedOptionAttribute', 'callbackCOA'));
-$GLOBALS['ISO_ATTR']['bbit_iso_coa']['class']	= 'FormIsotopeChargedOptionAttribute';
+$GLOBALS['ISO_ATTR']['bbit_iso_coa']['backend']		= 'multiColumnWizard';
+$GLOBALS['ISO_ATTR']['bbit_iso_coa']['sql']			= 'blob NULL';
+$GLOBALS['ISO_ATTR']['bbit_iso_coa']['callback'][]	= array('IsotopeChargedOptionAttribute', 'callbackCOA');
+$GLOBALS['ISO_ATTR']['bbit_iso_coa']['class']		= 'FormIsotopeChargedOptionAttribute';
